c27e7b7c16ed6554ea80e9f400544adf6859ea13
yavuzhan <gtacinq45@gmail.com>
2023-07-07 22:24:44 +0200

    07

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,1 +1,16 @@
-const mousemouve = 
+const navbar = document.querySelector("navbar");
+
+window.addEventListener("scroll" , () => {
+   // console.log("scroll test !!");
+   console.log(window.scrollY);
+if (window.scrollY > 120) {
+    navbar.style.top = 0;
+    navbar.style.position = 'fixed';
+    navbar.style.width = "100px";
+
+} 
+
+
+    
+});
+
