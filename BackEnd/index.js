9d15fa85da2f7d67a64bd8dc544c37fbc441967f
yavuzhan <gtacinq45@gmail.com>
2023-08-19 17:47:39 +0200

    cntr s

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,13 +1,41 @@
+const express = require("express");
+const app = express();
+const sequelize = require('./config/database')
 
-const http = require('http');
+app.use(express.json());
 
-const app = require('./app');
+// config ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+app.use('/home', require('./models/models'));
+sequelize.sync()
+.then(() => {
+    console.log('Nickel');
+})
+.catch((err) => {
+    console.log(erreur);
+});
 
+// BDD 
 const mysql = require('mysql');
 const connect = mysql.createConnection({
-    host:'localhost',
-    user:'root',
+    host: "localhost",
+    user: "root",
     password: "",
     database: "projet_dwwm",
 });
+
+app.use(express.static("public"));
+
+app.set('view engine', "ejs");
+app.set('views', "./views");
+app.listen(8080, ()=>{
+    console.log("le serveur tourne sur la page http://127.0.0.1:8080")
+});
+
+app.use(express.json());
+
+module.exports = {
+
+}
---
+++ BackEnd/models/models.js
@@ -0,0 +1,4 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const
